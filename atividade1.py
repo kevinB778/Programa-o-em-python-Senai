d97084a27c17785c8c846ad92db38dc544cbbfe5
kevinB778 <kevinhobeltramelo@gmail.com>
2025-05-22 17:46:55 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,53 @@
+#comentário
+#dados primitivos
+#para carregar o código, clique em f5
+#strings  = textos
+
+print('ola mundo!')
+print('Sejam bem vindos', 'Pessoas')
+print('Olá ', 'meu nome é', 'Gustavo')
+
+# dado do tipo inteiro(int)
+
+print(100)
+print(70)
+print(10)
+
+
+
+# dado do tipo real (float)
+print(1.5)
+print(10.0)
+print(30.5)
+
+
+
+# dado co tipo boleano(bool)
+
+print(True)
+print(False)
+
+
+#input() = guardar um dado
+
+print(input('Digite seu nome: '))
+
+# variáveis :
+
+nome = 'KEVIN'
+idade = 15
+endereco = 'Rua 25, Continental'
+curso = 'Programação em Python'
+
+print('Olá meu nome é', nome)
+print('minha idade é ', idade)
+print('minha casa está no endereco', endereco)
+print('Faço o curso', curso)
+
+
+# calculadora de soma
+
+numero_1 = int(input('digite um número'))
+numero_2 = int(input('Outro número'))
+
+print(numero_1 + numero_2)
