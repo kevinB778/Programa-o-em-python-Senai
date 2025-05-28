486ace934dc8e062e13a2a22be37e94c2e571e59
kevinB778 <kevinhobeltramelo@gmail.com>
2025-05-28 17:12:02 -0300

    Add files via upload

---
+++ +exercicios da aula6.py
@@ -0,0 +1,39 @@
+#Calcule o módulo ( % ) de 17 por 4.
+calcule = (17 % 4)
+print('resultado:',calcule)
+
+#Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+calcule = (9 * 3)**2
+print('resultado:', calcule)
+
+#Calcule a raiz quadrada de 81.
+calcule = (81 ** 0.5)
+print('resultado:',calcule)
+
+#Adicione 20 a 3 vezes 4.
+calcule = (20+(3*4))
+print('resultado:',calcule)
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+calcule = ((15 * 2) -7)
+print('resultado:',calcule)
+
+#Eleve 5 ao cubo.
+calcule = (5 ** 3)
+print('resultado:',calcule)
+
+#Calcule a média de 17, 21 e 25.
+media = ((17 + 21 + 25)/3)
+print('resultado:',media)
+
+#Multiplique 11 por 2 e adicione 7.
+calcule = ((11 * 2) +7)
+print('resultado:',calcule)
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+calcule = ((3 * 8)-15)/2
+print('resultado:',calcule)
+
+#Eleve 2 à potência de 10.
+calcule = (2**10)
+print('resultado:',calcule)
---
+++ aula6.py
@@ -0,0 +1,22 @@
+palavra = 'ISSO É UM TESTE EM PYTHON'
+
+comprimento = len(palavra)
+
+print(comprimento)
+
+lista = [2,1,3,4]
+print(lista)
+
+lista = [38000]
+print(lista)
+
+lista =  list(palavra)
+print(lista)
+
+tupla = ('ana' , 'Pedro')
+print(tupla)
+
+#range()
+
+lista = list(range(1,11,2))
+
