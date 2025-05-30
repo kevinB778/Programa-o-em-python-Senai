53aff3c1beba635dfa1e6ca98b3c714258f64058
kevinB778 <kevinhobeltramelo@gmail.com>
2025-05-30 15:34:44 -0300

    Add files via upload

---
+++ aula8.4.py
@@ -0,0 +1,10 @@
+idade  =  int(input('Idade: '))
+# condicional simples *** 
+if idade >= 16:
+   print('Você pode votar')
+
+if idade <16:
+    print('Não pode votar')
+
+if idade >=65:
+    print('Não precisa votar')
