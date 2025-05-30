e716d38638d49bd49e4939f0d34bedfe6442af43
kevinB778 <kevinhobeltramelo@gmail.com>
2025-05-30 17:55:34 -0300

    Add files via upload

--- sistema_hotel.py
+++ sistema_hotel.py
@@ -27,23 +27,39 @@ senha_acesso = input('Digite sua senha >> ')
 
 if login_nome in banco_dados['nomes'] and senha_acesso in banco_dados['senhas']:
     print('Seja bem vindo ao sistema!')
+    
+    print('Escolha o quarto: ', banco_dados['nomes'][0])
+    quartos  = ['','simples', 'duplo', 'luxo']
+    valores  =  ['',100,150,250]
+    print('quartos:  ', quartos)
+    escolha  =  int(input('id 1  simples | id 2 duplo | id 3  luxo')) 
+    qd  =  int(input('Quantidade de dias '))
+    calculo = qd * valores[escolha]
+    print('Vocês escolheu o quarto', quartos[escolha], 'Quant. Dias', qd)
+    print('Total a pagar R$', round(calculo,2))
+    
+    print('Escolha o quarto: ', banco_dados['nomes'][1])
+    quartos  = ['','simples', 'duplo', 'luxo']
+    valores  =  ['',100,150,250]
+    print('quartos:  ', quartos)
+    escolha  =  int(input('id 1  simples | id 2 duplo | id 3  luxo')) 
+    qd  =  int(input('Quantidade de dias '))
+    calculo = qd * valores[escolha]
+    print('Vocês escolheu o quarto', quartos[escolha], 'Quant. Dias', qd)
+    print('Total a pagar R$', round(calculo,2))    
+    
+    print('Escolha o quarto: ', banco_dados['nomes'][2])
+    quartos  = ['','simples', 'duplo', 'luxo']
+    valores  =  ['',100,150,250]
+    print('quartos:  ', quartos)
+    escolha  =  int(input('id 1  simples | id 2 duplo | id 3  luxo')) 
+    qd  =  int(input('Quantidade de dias '))
+    calculo = qd * valores[escolha]
+    print('Vocês escolheu o quarto', quartos[escolha], 'Quant. Dias', qd)
+    print('Total a pagar R$', round(calculo,2))
+    
 else:
     print('Acesso negado, tente novamente!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
