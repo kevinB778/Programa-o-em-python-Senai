e2d6b21e46462c0ee65e309bc7dd11f9c683b3a2
kevinB778 <kevinhobeltramelo@gmail.com>
2025-06-13 15:51:37 -0300

    Add files via upload

---
+++ Aula18.py
@@ -0,0 +1,20 @@
+#                                          SCRUM MASTER
+# Scrum é um framework de gerenciamento que as equipes usam para se auto-organizar e trabalhar em direção a um objetivo em comum.
+# O Scrum é uma estrutura ágil que permite que as equipes de desenvolvimento de software (e outras áreas) gerenciem projetos de forma eficiente e adaptável,
+#  focando na entrega de valor ao cliente de forma iterativa e incremental. 
+# Sua importância reside na capacidade de promover a auto-organização da equipe, a transparência do processo,
+#  a inspeção contínua e a adaptação às mudanças, resultando em projetos mais flexíveis, produtivos e com melhor qualidade. 
+#  O Scrum é baseado em ciclos curtos de trabalho chamados Sprints, que geralmente duram de duas a quatro semanas
+
+#                                             PO
+# é o responsável por maximizar o valor do produto resultante do trabalho da equipe Scrum.
+# Ele é o elo entre a equipe de desenvolvimento e os stakeholders, garantindo que o produto atenda às necessidades e expectativas dos usuários. 
+
+#                                           Líder equipe de TI
+# é um profissional que guia e coordena as atividades de uma equipe de TI, responsável por desenvolver, operar e manter a infraestrutura tecnológica de uma empresa.
+# A sua importância reside em garantir a eficiência, a segurança e o alinhamento da TI com os objetivos de negócio da organização. 
+
+#                                           Equipe de tecnologia
+# é um grupo de profissionais especializado que gerencia, desenvolve e mantém sistemas de informação e a infraestrutura tecnológica de uma organização.
+#  Sua importância reside na necessidade de garantir que a tecnologia, essencial para a eficiência e o sucesso de qualquer empresa moderna, funcione corretamente e esteja sempre atualizada. 
+
